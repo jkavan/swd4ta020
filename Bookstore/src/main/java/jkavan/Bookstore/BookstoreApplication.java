d4f735ffe68993896e660f8e9cc99265b43a46f9
Jani <jani@kavander.fi>
2022-09-01 19:57:01 +0300

    Exercise 3.1

--- Bookstore/src/main/java/jkavan/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/jkavan/Bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package jkavan.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import jkavan.Bookstore.domain.Book;
+import jkavan.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,16 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {					
+			repository.save(new Book("Dracula", "Bram Stoker", 1897, "1503261387", 9));
+			repository.save(new Book("Kubernetes: Up and Running", "Brendan Burns", 2019, "9781492046530", 58));
+			
+			for (Book book : repository.findAll()) {
+				System.out.println(book);
+			}
+		};
+	}
+	
 }
--- Bookstore/src/main/java/jkavan/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/jkavan/Bookstore/domain/Book.java
@@ -1,13 +1,27 @@
 package jkavan.Bookstore.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String title;
 	private String author;
+	@Column(name = "`year`")
 	private int year;
 	private String isbn;
-	private float price;
+	private int price;
 	
-	public Book(String title, String author, int year, String isbn, float price) {
+	public Book() {}
+	
+	public Book(String title, String author, int year, String isbn, int price) {
 		super();
 		this.title = title;
 		this.author = author;
@@ -52,7 +66,13 @@ public class Book {
 		return price;
 	}
 	
-	public void setPrice(float price) {
+	public void setPrice(int price) {
 		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
 	}
 }
