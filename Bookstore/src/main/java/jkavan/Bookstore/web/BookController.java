acf4aa205b8ef7911385340e2c2bc819c90d2140
Jani <jani@kavander.fi>
2022-09-02 19:21:11 +0300

    Exercise 3.2

--- Bookstore/src/main/java/jkavan/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/jkavan/Bookstore/web/BookController.java
@@ -1,12 +1,27 @@
 package jkavan.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jkavan.Bookstore.domain.Book;
+import jkavan.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository;
+	
 	@RequestMapping("/index")
 	public String home() {
 		return "index"; 
+	}
+	
+	@GetMapping("/booklist")
+	public String booklist(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist"; 
 	}
 }
