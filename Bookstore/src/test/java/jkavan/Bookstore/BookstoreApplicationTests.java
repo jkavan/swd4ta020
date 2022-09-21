513a13119e8c15fdc372626e32a4853a5f1c9fbe
Jani <jani@kavander.fi>
2022-09-21 19:59:49 +0300

    Exercise 7.1a

--- Bookstore/src/test/java/jkavan/Bookstore/BookstoreApplicationTests.java
+++ Bookstore/src/test/java/jkavan/Bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,27 @@
 package jkavan.Bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jkavan.Bookstore.web.BookController;
+import jkavan.Bookstore.web.CategoryController;
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+	private BookController bookController;
+	
+	@Autowired
+	private CategoryController categoryController;
+	
 	@Test
 	void contextLoads() {
+		assertThat(bookController).isNotNull();
+		assertThat(categoryController).isNotNull();
 	}
 
 }
